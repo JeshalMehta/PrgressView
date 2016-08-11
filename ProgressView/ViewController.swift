772f92c529250b763c2788586c539b7452fa002f
Jeshal Mehta <jeshalmehta@Jeshals-MacBook-Pro.local>
2016-08-11 08:12:58 +0530

    progress view

--- ProgressView/ViewController.swift
+++ ProgressView/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
-
+var p : Double = 20.0
 class ViewController: UIViewController {
 
+    var timer : NSTimer!
+    
+    @IBAction func fireTheEvent(sender: AnyObject) {
+    
+        timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: #selector(ViewController.UpdateTheProgress), userInfo: nil, repeats: true)
+        timer.fire()
+        
+        
+        
+       
+    
+    }
+    @IBOutlet weak var ProgressView: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+       self.ProgressView.trackTintColor = UIColor.brownColor()
+   self.ProgressView.progressTintColor = UIColor.blueColor()
+    
+    
+    }
+    
+    func UpdateTheProgress()
+    {
+         self.ProgressView.progress = (Float)(p/100.0)
+          p = p + 10.0
+     }
+    
+    
+    
+    
+    
 
 }
 
